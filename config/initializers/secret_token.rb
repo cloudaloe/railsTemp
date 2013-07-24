8ac46002aab39a1ad4e819f94e59f91cc50cd099
matan <matan@cloudaloe.org>
2013-07-24 23:56:37 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '6da8b9fdcafffa58b482525518ae4d559bc47d8b87768593961dc1ea034e7229f3c384976d3f4d6bc57d01a967fd410c1f421ea0ffa49476af479a1fc4cb81cb'
